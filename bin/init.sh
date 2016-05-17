bc686b65ac9939e1e417951efca8958094a40b42
Josh King <jking@chambana.net>
2016-05-16 20:37:22 -0400

    Variable handling and formatting tweaks to init script.

--- bin/init.sh
+++ bin/init.sh
@@ -5,38 +5,39 @@
 CHECK_BIN sed
 CHECK_BIN prosodyctl
 CHECK_VAR XMPP_DOMAIN
-CHECK_VAR DB_HOST
-CHECK_VAR DB_PORT
-CHECK_VAR DB_NAME
-CHECK_VAR DB_USER
 CHECK_VAR DB_PASS
-CHECK_VAR LDAP_HOST
 CHECK_VAR LDAP_DN
 CHECK_VAR LDAP_PASS
-CHECK_VAR LDAP_GROUP
 CHECK_VAR LDAP_USER_BASE
 CHECK_VAR LDAP_GROUP_BASE
 
 ADMINS=${ADMINS:-""}
+DB_HOST=${DB_HOST:-"postgres"}
+DB_PORT=${DB_PORT:-"5432"}
+DB_USER=${DB_USER:-"prosody"}
+DB_NAME=${DB_NAME:-"prosody"}
+LDAP_HOST=${LDAP_HOST:-"ldap"}
+LDAP_GROUP=${LDAP_GROUP:-"xmpp"}
 
 MSG "Configuring Prosody..."
 
 sed -i -e "s/{{ADMINS}}/${ADMINS}/" \
-  -e "s/{{XMPP_DOMAIN}}/${XMPP_DOMAIN}/" \
-  -e "s/{{DB_HOST}}/${DB_HOST}/" \ 
-  -e "s/{{DB_PORT}}/${DB_PORT}/" \
-  -e "s/{{DB_NAME}}/${DB_NAME}/" \
-  -e "s/{{DB_USER}}/${DB_USER}/" \
-  -e "s/{{DB_PASS}}/${DB_PASS}/" \
-  /etc/prosody/prosody.cfg.lua
+	-e "s/{{XMPP_DOMAIN}}/${XMPP_DOMAIN}/" \
+	-e "s/{{DB_HOST}}/${DB_HOST}/" \ 
+	-e "s/{{DB_PORT}}/${DB_PORT}/" \
+	-e "s/{{DB_NAME}}/${DB_NAME}/" \
+	-e "s/{{DB_USER}}/${DB_USER}/" \
+	-e "s/{{DB_PASS}}/${DB_PASS}/" \
+	/etc/prosody/prosody.cfg.lua
 
 sed -i -e "s/{{LDAP_HOST}}/${LDAP_HOST}/" \
-  -e "s/{{LDAP_DN}}/${LDAP_DN}/" \
-  -e "s/{{LDAP_PASS}}/${LDAP_PASS}/" \ 
-  -e "s/{{LDAP_GROUP}}/${LDAP_GROUP}/" \
-  -e "s/{{LDAP_USER_BASE}}/${LDAP_USER_BASE}/" \
-  -e "s/{{LDAP_GROUP_BASE}}/${LDAP_GROUP_BASE}/" \
-  /etc/prosody/prosody-ldap.cfg.lua
+	-e "s/{{LDAP_DN}}/${LDAP_DN}/" \
+	-e "s/{{LDAP_PASS}}/${LDAP_PASS}/" \ 
+	-e "s/{{LDAP_GROUP}}/${LDAP_GROUP}/" \
+	-e "s/{{LDAP_USER_BASE}}/${LDAP_USER_BASE}/" \
+	-e "s/{{LDAP_GROUP_BASE}}/${LDAP_GROUP_BASE}/" \
+	/etc/prosody/prosody.cfg.lua
+  
 
 echo VirtualHost \"${XMPP_DOMAIN}\" >> /etc/prosody/conf.d/domain.cfg.lua
 echo "	ssl = {" >> /etc/prosody/conf.d/domain.cfg.lua
